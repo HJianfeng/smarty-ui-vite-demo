c1c2de314359ff152e02e8ba31e4fe50a8b7b30d
hjf <625598479@qq.com>
2022-10-21 11:55:53 +0800

    chapter 2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-import { createApp } from "vue";
+import { createApp } from 'vue/dist/vue.cjs';
+import SmartyUI from "./entry";
 
-import { Button } from "./components/Button";
-
-createApp(Button).mount("#app")
+createApp({
+  template:`
+  <div>
+    <SButton color="blue">蓝色按钮</SButton>
+    <SButton color="green">绿色按钮</SButton>
+    <SButton color="gray">灰色按钮</SButton>
+    <SButton color="yellow">黄色按钮</SButton>
+    <SButton color="red">红色按钮</SButton>
+ </div>
+  `
+})
+.use(SmartyUI)
+.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue"
+import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import Unocss from "./config/unocss";
 // https://vitejs.dev/config/*
 
 const rollupOptions = {
@@ -13,10 +15,13 @@ const rollupOptions = {
 };
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), Unocss(), vueJsx()
+  ],
   build: {
     rollupOptions,
     minify:false,
+    cssCodeSplit: true,
     lib: {
       entry: "./src/entry.ts",
       name: "SmartyUI",
