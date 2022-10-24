39b959c8774a1c89e0de774c786191882f590e98
hjf <625598479@qq.com>
2022-10-24 14:10:03 +0800

    feat: Button测试用例

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Unocss from "./config/unocss";
+
 // https://vitejs.dev/config/*
 
 const rollupOptions = {
@@ -29,5 +30,16 @@ export default defineConfig({
       // 导出模块格式
       formats: ["esm", "umd","iife"],
     },
+  },
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
   }
 });
